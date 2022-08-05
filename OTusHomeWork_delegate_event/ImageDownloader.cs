1e5c8faa1c6ae41f119c498b3b01e0d42d66fc70
Vladimir Melnikov <dybenko36@gmail.com>
2022-08-05 10:30:11 +0300

    Добавьте файлы проекта.

---
+++ OTusHomeWork_delegate_event/ImageDownloader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+
+namespace OTusHomeWork_delegate_event
+{
+    public class ImageDownloader
+    {
+        public delegate void ImageCompleted();
+
+        public delegate void ImageStarted();
+        public event ImageCompleted imageCompleted;
+
+        public event ImageStarted imageStarted;
+        public void Download()
+        {
+            // Откуда будем качать
+            string remoteUri = "https://effigis.com/wp-content/uploads/2015/02/Iunctus_SPOT5_5m_8bit_RGB_DRA_torngat_mountains_national_park_8bits_1.jpg";
+            // Как назовем файл на диске
+            string fileName = "bigimage.jpg";
+
+            // Качаем картинку в текущую директорию
+            var myWebClient = new WebClient();
+            Console.WriteLine("Качаю \"{0}\" из \"{1}\" .......\n\n", fileName, remoteUri);
+
+            imageStarted?.Invoke();
+            myWebClient.DownloadFileTaskAsync(remoteUri, fileName);
+            imageCompleted?.Invoke();
+
+            Console.WriteLine("Успешно скачал \"{0}\" из \"{1}\"", fileName, remoteUri);
+        }
+
+    }
+}
---
+++ OTusHomeWork_delegate_event/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+
+namespace OTusHomeWork_delegate_event
+{
+    public class Program
+    {
+        //private static event ImageStarted;
+        private static void Main(string[] args)
+        {
+            var imageDownloader = new ImageDownloader();
+            imageDownloader.imageStarted += ImageDownloader_imageStarted;
+            imageDownloader.imageCompleted += ImageDownloader_imageCompleted;
+            imageDownloader.Download();
+
+            Console.WriteLine("Нажмите любую клавишу для выхода");
+            Console.ReadLine();
+        }
+
+        private static void ImageDownloader_imageCompleted()
+        {
+            Console.WriteLine("Скачивание файла закончилось");
+        }
+
+        private static void ImageDownloader_imageStarted()
+        {
+            Console.WriteLine("Скачивание файла началось");
+        }
+    }
+}
+/*
+ * сделаны 1-3
+ * https://otus.ru/learning/168620/
+ */
