5c68520d880ff8f09b8c31dca9531e068f5aaaa2
Vladimir Melnikov <dybenko36@gmail.com>
2022-08-14 15:42:35 +0300

    20220814_15:42

---
+++ OTusHomeWork_delegate_event/ImageDownloader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace OTusHomeWork_delegate_event
+{
+    public static class ImageDownloader
+    {
+        public delegate void ImageCompleted();
+        public delegate void ImageStarted();
+
+        public static event ImageCompleted Completed;
+        public static event ImageStarted Started;
+
+        public static async Task Download()
+        {
+            string remoteUri = "https://effigis.com/wp-content/uploads/2015/02/Iunctus_SPOT5_5m_8bit_RGB_DRA_torngat_mountains_national_park_8bits_1.jpg";
+            string fileName = "bigimage.jpg";
+
+            WebClient myWebClient = new WebClient();
+            Console.WriteLine("Качаю \"{0}\" из \"{1}\" .......\n\n", fileName, remoteUri);
+
+            Started?.Invoke();
+            await myWebClient.DownloadFileTaskAsync(remoteUri, fileName);
+            Completed?.Invoke();
+
+            Console.WriteLine("Успешно скачал \"{0}\" из \"{1}\"", fileName, remoteUri);
+        }
+    }
+}
--- OTusHomeWork_delegate_event/Program.cs
+++ OTusHomeWork_delegate_event/Program.cs
@@ -1,17 +1,18 @@
 ﻿using System;
-using System.Net;
 using System.Threading.Tasks;
 
 namespace OTusHomeWork_delegate_event
 {
-    class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
-            var imageDownloader = new ImageDownloader();
+            //var imageDownloader = new ImageDownloader();
+            ImageDownloader.Started += ImageDownloader_Started;
+            ImageDownloader.Completed += ImageDownloader_Completed;
 
-            imageDownloader.Started += ImageDownloader_Started;
-            imageDownloader.Completed += ImageDownloader_Completed;
+            //imageDownloader.Started += ImageDownloader_Started;
+            //imageDownloader.Completed += ImageDownloader_Completed;
 
             Task task = ImageDownloader.Download();
 
@@ -25,41 +26,15 @@ namespace OTusHomeWork_delegate_event
                 Console.WriteLine(status);
             }
             Console.WriteLine("Выполнение завершено");
-            return;
         }
-
-        private static void ImageDownloader_Completed()
+        public static void ImageDownloader_Completed()
         {
             Console.WriteLine("Скачивание файла закончилось");
         }
 
-        private static void ImageDownloader_Started()
+        public static void ImageDownloader_Started()
         {
             Console.WriteLine("Скачивание файла началось");
-        }
-    }
-
-    public class ImageDownloader
-    {
-        public delegate void imageCompleted();
-        public delegate void imageStarted();
-
-        public event imageCompleted Completed;
-        public event imageStarted Started;
-
-        public async static Task Download()
-        {
-            string remoteUri = "https://effigis.com/wp-content/uploads/2015/02/Iunctus_SPOT5_5m_8bit_RGB_DRA_torngat_mountains_national_park_8bits_1.jpg";
-            string fileName = "bigimage.jpg";
-
-            WebClient myWebClient = new WebClient();
-            Console.WriteLine("Качаю \"{0}\" из \"{1}\" .......\n\n", fileName, remoteUri);
-
-            Started?.Invoke();
-            await myWebClient.DownloadFileTaskAsync(remoteUri, fileName);
-            Completed?.Invoke();
-
-            Console.WriteLine("Успешно скачал \"{0}\" из \"{1}\"", fileName, remoteUri);
         }
     }
 }
